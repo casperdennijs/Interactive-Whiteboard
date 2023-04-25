a6952d683f81e3151e8e50ccf9a0883d7e88825e
Casper <casper.dennijs@outlook.com>
2023-04-25 11:26:45 +0200

    Show cursors

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const port = process.env.PORT || 4242;
 
+const clients = {};
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -16,12 +18,14 @@ server.listen(port, () => {
     console.log('listening on *:' + port);
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
+const addClient = socket => {
+    console.log("New client connected", socket.id);
+    clients[socket.id] = socket;
+};
+const removeClient = socket => {
+    console.log("Client disconnected", socket.id);
+    delete clients[socket.id];
+};
 
 io.on('connection', (socket) => {
     socket.on('send-nickname', (nickname) => {
@@ -43,4 +47,20 @@ io.on('connection', (socket) => {
 
 io.on('connection', (socket) => {
     socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+});
+
+io.on("connection", (socket) => {
+    let id = socket.id;
+  
+    addClient(socket);
+  
+    socket.on("mousemove", data => {
+      data.id = id;
+      socket.broadcast.emit("moving", data);
+    });
+  
+    socket.on("disconnect", () => {
+      removeClient(socket);
+      socket.broadcast.emit("clientdisconnect", id);
+    });
 });
